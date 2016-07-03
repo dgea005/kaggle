57b6b5d34a750cb3ab28c311a96d1d0561844f1d
dgea005 <danielgeals@gmail.com>
2016-07-03 16:35:08 +0200

    updating preprocessing

--- west_nile_virus/data_processing.R
+++ west_nile_virus/data_processing.R
@@ -1,8 +1,12 @@
-# need to arange data to be usable
+##
+## script to preprocess data for WNV
+## imputes missing values, converts types and add features
+##
+
+
 library(plyr)
 library(dplyr)
 library(lubridate)
-
 
 
 # read in data
@@ -12,9 +16,17 @@ weather = read.csv(file.path(data_dir, "weather.csv"))
 spray = read.csv(file.path(data_dir, "spray.csv"))
 test = read.csv(file.path(data_dir, "test.csv"))
 
-# preprocessing of weather.csv data
-# parses description, fill missing data ,etc.
+##
+## preprocessing of weather.csv data
+## parses description, fill missing data ,etc.
+##
+
 source("weather_preprocess.R")
+
+
+##
+## date conversion
+##
 
 add_date_information <- function(data){
   data$Date = levels(data$Date)[as.numeric(data$Date)]
@@ -76,10 +88,9 @@ train = add_species_dummies(train)
 test = add_species_dummies(test)
 
 
-
-# average of 1 month temperature for each obs
-# the 1 refers to this being of station1 data
-# this was 28 in best model
+##
+## create weather features
+## 
 
 
 ## window function to create weather from previous x days of weather data
@@ -93,16 +104,170 @@ create_variable_from_weather <- function(data, wthr_dt, weather_var, new_col_nam
   return(data)
 }
 
-# previous month statistics
+## average temperatures: one month rolling window
 
 train = create_variable_from_weather(train, station1, 'Tavg', 'Tavg1_mAv', 28, mean)
 train = create_variable_from_weather(train, station1, 'Tavg', 'Tavg1_mS', 28, sd)  #std deviation
 train = create_variable_from_weather(train, station2, 'Tavg', 'Tavg2_mAv', 24, mean)
-
-test = create_variable_from_weather(test, station1, 'Tavg', 'Tavg1_mAv', 2, mean)
+test = create_variable_from_weather(test, station1, 'Tavg', 'Tavg1_mAv', 28, mean)
 test = create_variable_from_weather(test, station1, 'Tavg', 'Tavg1_mS', 28, sd)  #std deviation
 test = create_variable_from_weather(test, station2, 'Tavg', 'Tavg2_mAv', 24, mean)
 
+
+
+## precipitation: one week rolling window
+train = create_variable_from_weather(train, station1, 'PrecipTotal', 'Tavg1_prcpAv', 7, mean)
+train = create_variable_from_weather(train, station1, 'PrecipTotal', 'Tavg1_prcpS', 7, sd)
+test = create_variable_from_weather(test, station1, 'PrecipTotal', 'Tavg1_prcpAv', 7, mean)
+test = create_variable_from_weather(test, station1, 'PrecipTotal', 'Tavg1_prcpS', 7, sd)
+
+
+## min and max temperatures: one week rolling window
+train = create_variable_from_weather(train, station1, 'Tmin', 'Tmin1_wk', 7, min)
+train = create_variable_from_weather(train, station1, 'Tmax', 'Tmax1_wk', 7, max)
+test = create_variable_from_weather(test, station1, 'Tmin', 'Tmin1_wk', 7, min)
+test = create_variable_from_weather(test, station1, 'Tmax', 'Tmax1_wk', 7, max)
+
+
+## rain during week: rolling window
+
+
+
+
+# changed 7 to 10
+for (i in 1:length(unique(train$Date))){
+  obs_date = unique(train$Date)[i]
+  data_subset = station1[station1$Date <= obs_date & (station1$Date >= obs_date - days(11)), ]
+  train[train$Date == obs_date, "rain_wk"] = as.numeric(sum(data_subset$PrecipTotal))
+}
+
+
+for (i in 1:length(unique(train$Date))){
+  obs_date = unique(train$Date)[i]
+  data_subset = station1[station1$Date <= obs_date & (station1$Date >= obs_date - days(30)), ]
+  train[train$Date == obs_date, "rain_wk15"] = as.numeric(sum(data_subset$PrecipTotal))
+}
+
+for (i in 1:length(unique(test$Date))){
+  obs_date = unique(test$Date)[i]
+  data_subset = station1[station1$Date <= obs_date & (station1$Date >= obs_date - days(12)), ]
+  test[test$Date == obs_date, "rain_wk"] = as.numeric(sum(data_subset$PrecipTotal))
+}
+
+
+
+# need to explore DewPoint and WetBulb
+# found 15 days was the best for this
+for (i in 1:length(unique(train$Date))){
+  obs_date = unique(train$Date)[i]
+  data_subset = station1[station1$Date <= obs_date & (station1$Date >= obs_date - days(15)), ]
+  train[train$Date == obs_date, "dew_wk"] = mean(data_subset$DewPoint)
+}
+
+for (i in 1:length(unique(test$Date))){
+  obs_date = unique(test$Date)[i]
+  data_subset = station1[station1$Date <= obs_date & (station1$Date >= obs_date - days(15)), ]
+  test[test$Date == obs_date, "dew_wk"] = mean(data_subset$DewPoint)
+}
+
+
+# dew sd
+
+for (i in 1:length(unique(train$Date))){
+  obs_date = unique(train$Date)[i]
+  data_subset = station1[station1$Date <= obs_date & (station1$Date >= obs_date - days(15)), ]
+  train[train$Date == obs_date, "dew_sd"] = sd(data_subset$DewPoint)
+}
+
+for (i in 1:length(unique(test$Date))){
+  obs_date = unique(test$Date)[i]
+  data_subset = station1[station1$Date <= obs_date & (station1$Date >= obs_date - days(15)), ]
+  test[test$Date == obs_date, "dew_sd"] = sd(data_subset$DewPoint)
+}
+
+
+# wetbulb
+for (i in 1:length(unique(train$Date))){
+  obs_date = unique(train$Date)[i]
+  data_subset = station1[station1$Date <= obs_date & (station1$Date >= obs_date - days(4)), ]
+  train[train$Date == obs_date, "wb_wk"] = mean(data_subset$WetBulb)
+}
+
+
+for (i in 1:length(unique(test$Date))){
+  obs_date = unique(test$Date)[i]
+  data_subset = station1[station1$Date <= obs_date & (station1$Date >= obs_date - days(4)), ]
+  test[test$Date == obs_date, "wb_wk"] = mean(data_subset$WetBulb)
+}
+
+for (i in 1:length(unique(train$Date))){
+  obs_date = unique(train$Date)[i]
+  data_subset = station1[station1$Date <= obs_date & (station1$Date >= obs_date - days(10)), ]
+  train[train$Date == obs_date, "wb_sd"] = sd(data_subset$WetBulb)
+}
+
+
+for (i in 1:length(unique(test$Date))){
+  obs_date = unique(test$Date)[i]
+  data_subset = station1[station1$Date <= obs_date & (station1$Date >= obs_date - days(10)), ]
+  test[test$Date == obs_date, "wb_sd"] = sd(data_subset$WetBulb)
+}
+
+
+
+
+
+for (i in 1:length(unique(train$Date))){
+  obs_date = unique(train$Date)[i]
+  data_subset = station1[station1$Date <= obs_date & (station1$Date >= obs_date - days(10)), ]
+  train[train$Date == obs_date, "speed_wk"] = mean(data_subset$AvgSpeed)
+}
+
+for (i in 1:length(unique(test$Date))){
+  obs_date = unique(test$Date)[i]
+  data_subset = station1[station1$Date <= obs_date & (station1$Date >= obs_date - days(10)), ]
+  test[test$Date == obs_date, "speed_wk"] = mean(data_subset$AvgSpeed)
+}
+
+for (i in 1:length(unique(train$Date))){
+  obs_date = unique(train$Date)[i]
+  data_subset = station1[station1$Date <= obs_date & (station1$Date >= obs_date - days(7)), ]
+  train[train$Date == obs_date, "speed_sd"] = sd(data_subset$AvgSpeed)
+}
+
+for (i in 1:length(unique(test$Date))){
+  obs_date = unique(test$Date)[i]
+  data_subset = station1[station1$Date <= obs_date & (station1$Date >= obs_date - days(7)), ]
+  test[test$Date == obs_date, "speed_sd"] = sd(data_subset$AvgSpeed)
+}
+# 10 best so far for speed_sd
+
+
+# minutes of sunlight
+# want to try moving average i.e., average from 21-28 days ago
+
+
+for (i in 1:length(unique(train$Date))){
+  obs_date = unique(train$Date)[i]
+  data_subset = station1[station1$Date <= obs_date - days(25) & (station1$Date >= obs_date - days(28)), ]
+  train[train$Date == obs_date, "sunlight"] = mean(data_subset$Sun_min)
+}
+
+
+for (i in 1:length(unique(test$Date))){
+  obs_date = unique(test$Date)[i]
+  data_subset = station1[station1$Date <= obs_date - days(0) & (station1$Date >= obs_date - days(7)), ]
+  test[test$Date == obs_date, "sunlight"] = mean(data_subset$Sun_min)
+}
+
+
+# direction?
+
+for (i in 1:length(unique(train$Date))){
+  obs_date = unique(train$Date)[i]
+  data_subset = station1[station1$Date <= obs_date & (station1$Date >= obs_date - days(7)), ]
+  train[train$Date == obs_date, "dir"] = sd(data_subset$ResultDir)
+}
 
 
 
